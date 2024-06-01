b850031238c4ca585d35e83e618eaeba5b5f2cdd
falbas <farhanalbas30@gmail.com>
2024-06-01 16:32:22 +0700

    add comment

--- main.py
+++ main.py
@@ -14,27 +14,30 @@ def find_nearest(array, value):
     return idx
 
 
-# plotting ecmwf ifs data
+# [plotting ecmwf ifs data]
 
+# get date now
 dt = datetime.datetime.now()
-# date = dt.strftime("%Y-%m-%d")
-date = "2024-05-30"
+date = dt.strftime("%Y-%m-%d")
+# date = "2024-05-30"
 
+# download data from ecmwf ifs using herbie
 FH = FastHerbie(
     DATES=[date],
     model="ifs",
     product="oper",
     fxx=range(0, 147, 3),
 )
-
 search = ":[u|v]:1000:"
 FH.download(search)
 ds = FH.xarray(search)
 
+# get variable from grib
 valid_time = ds["valid_time"]
 lat = ds["latitude"][280:441]  # get lat 20, -10
 lon = ds["longitude"][1040:1361]  # get lon 80, 160
 
+# generate image of wind direction for yolo detection
 os.mkdir(f"input/{date}")
 for i in range(0, len(valid_time)):
     u = ds["u"][i][280:441, 1040:1361]
@@ -74,12 +77,13 @@ for i in range(0, len(valid_time)):
     plt.close()
 
 
-# detect the tropical cyclone
+# [detect the tropical cyclone]
 
 os.mkdir(f"output/{date}")
 f = open(f"result/{date}.csv", "w")
 image_list = os.listdir(f"input/{date}")
 
+# detect the tc and generate image with bounding box and save the result to csv
 model = YOLO("model/best.pt")
 for i in range(0, len(image_list)):
     name = image_list[i].split(".")[0]
@@ -95,35 +99,42 @@ for i in range(0, len(image_list)):
         object_name = model.names[cls]
         label = f"{object_name} {score}"
 
+        # add bounding box to the image
         cv2.rectangle(img, (x0, y0), (x1, y1), (255, 0, 0), 2)
         cv2.putText(
             img, label, (x0, y0 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2
         )
 
+        # add circle dot to the center of bounding box
         midx = int(x0 + ((x1 - x0) / 2))
         midy = int(y0 + ((y1 - y0) / 2))
         cv2.circle(img, (midx, midy), 1, (0, 0, 255), 2)
 
+        # get center latlon of the detection result
         w = result[0].orig_shape[1]
         h = result[0].orig_shape[0]
         mlat = round(float(20 - (midy * 40 / h)), 5)
         mlon = round(float((midx * 80 / w) + 80), 5)
 
+        # get bounding box latlon
         wlon = round(float((x0 * 80 / w) + 80), 5)
         nlat = round(float(20 - (y0 * 40 / h)), 5)
         elon = round(float((x1 * 80 / w) + 80), 5)
         slat = round(float(20 - (y1 * 40 / h)), 5)
 
+        # get index of grib latlon
         widx = find_nearest(lon.values, wlon)
         nidx = find_nearest(lat.values, nlat)
         eidx = find_nearest(lon.values, elon)
         sidx = find_nearest(lat.values, slat)
 
+        # get max wind speed of the detection result
         u = ds["u"][i][nidx:sidx, widx:eidx]
         v = ds["v"][i][nidx:sidx, widx:eidx]
         ws = np.sqrt(u**2 + v**2)
         maxws = round(float(ws.values.max() * 3.6), 2)
 
+        # write the result
         cv2.imwrite(f"output/{date}/{name}.jpg", img)
         f.write(f"{date},{name},{mlat},{mlon},{score},{maxws}\n")
 f.close()
